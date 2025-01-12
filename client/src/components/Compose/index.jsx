fc488f8f0dd9f63e0e62ea0df292c8b5bc50b7dd
Altamash Ahmad <altamashahmad910@gmail.com>
2025-01-13 00:25:10 +0530

    update

--- client/src/components/Compose/index.jsx
+++ client/src/components/Compose/index.jsx
@@ -6,7 +6,7 @@ import { IoIosClose } from 'react-icons/io';
 const Compose = () => {
   return (
     <div className='bg-white max-w-6xl shadow-xl shadow-slate-600 rounded-t-md'>
-      <div className='p-2 bg-[#F2F6FC] flex justify-between items-center'>
+      <div className='p-2 bg-[#F2F6FC] flex justify-between items-center rounded-t-md'>
         <h1 className='font-normal'>New Message</h1>
         <div className='flex items-center gap-1'>
           <span className='hover:bg-gray-200 hover:cursor-pointer p-[0.1rem] rounded-sm text-gray-700'><MdOutlineMinimize size={'20px'}/></span>
@@ -14,7 +14,11 @@ const Compose = () => {
           <span className='hover:bg-gray-200 hover:cursor-pointer p-[0.1rem] rounded-sm text-gray-700'><IoIosClose size={'20px'}/></span>
         </div>
       </div>
-      <div></div>
+      <form action="" className='flex flex-col p-3 gap-2'>
+        <input type="text" placeholder='To' className='border-b border-gray-300 '/>
+        <input type="text" placeholder='Subject' className='border-b border-gray-300 pb-1' />
+        <textarea rows={'10'} cols={'30'} className='outline-none py-1'></textarea>
+      </form>
     </div>
   )
 }
