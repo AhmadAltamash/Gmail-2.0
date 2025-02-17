55bf705a13c558579c6a21a642adb3a1399dc146
Altamash Ahmad <altamashahmad910@gmail.com>
2025-02-17 23:08:24 +0530

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/login", // Ensure correct base path
+  server: {
+    historyApiFallback: true, // Enables proper routing
+  },
 })
