5f0e43a2153dcb4cafc494f19f9438b5e9337c60
Altamash Ahmad <altamashahmad910@gmail.com>
2025-02-17 23:04:07 +0530

    update

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsOptions= {
-    origin: 'https://gmail-2-0-mu.vercel.app',
+    origin: 'https://gmail-2-0-1.onrender.com',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }
