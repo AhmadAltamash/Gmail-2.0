3bb71d20f56464bea01ef759937e96bb3e0f9d2a
Altamash Ahmad <altamashahmad910@gmail.com>
2025-02-17 23:26:20 +0530

    update

--- client/src/API/api.js
+++ client/src/API/api.js
@@ -1,11 +1,11 @@
 export const HOST = 'https://gmail-2-0.onrender.com';
 
 //Users
-export const REGISTER = `${HOST}/user/register`;
-export const LOGIN = `${HOST}/user/login`;
-export const LOGOUT = `${HOST}/user/logout`;
+export const REGISTER = `${HOST}api/v1/user/register`;
+export const LOGIN = `${HOST}api/v1/user/login`;
+export const LOGOUT = `${HOST}api/v1/user/logout`;
 
 //Mails
-export const SEND_MAIL = `${HOST}/email/createEmail`;
-export const GET_ALL_MAILS = `${HOST}/email/getAllEmails`;
-export const DELETE_MAIL = `${HOST}/email/email`;
+export const SEND_MAIL = `${HOST}api/v1/email/createEmail`;
+export const GET_ALL_MAILS = `${HOST}api/v1/email/getAllEmails`;
+export const DELETE_MAIL = `${HOST}api/v1/email/email`;
